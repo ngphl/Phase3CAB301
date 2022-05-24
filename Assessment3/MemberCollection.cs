c36cc9113a2d98d168779e79cd9daf3c724013aa
Long Nguyen <npl.long2001@gmail.com>
2022-05-24 14:00:15 +1000

    first commit

---
+++ Assessment3/MemberCollection.cs
@@ -0,0 +1,223 @@
+﻿//CAB301 assessment 1 - 2022
+//The implementation of MemberCollection ADT
+using System;
+using System.Linq;
+
+
+class MemberCollection : IMemberCollection
+{
+    // Fields
+    private int capacity;
+    private int count;
+    private Member[] members; //make sure members are sorted in dictionary order
+
+    // Properties
+
+    // get the capacity of this member colllection 
+    // pre-condition: nil
+    // post-condition: return the capacity of this member collection and this member collection remains unchanged
+    public int Capacity { get { return capacity; } }
+
+    // get the number of members in this member colllection 
+    // pre-condition: nil
+    // post-condition: return the number of members in this member collection and this member collection remains unchanged
+    public int Number { get { return count; } }
+
+
+
+
+    // Constructor - to create an object of member collection 
+    // Pre-condition: capacity > 0
+    // Post-condition: an object of this member collection class is created
+
+    public MemberCollection(int capacity)
+    {
+        if (capacity > 0)
+        {
+            this.capacity = capacity;
+            members = new Member[capacity];
+            count = 0;
+        }
+    }
+
+    // check if this member collection is full
+    // Pre-condition: nil
+    // Post-condition: return ture if this member collection is full; otherwise return false.
+    public bool IsFull()
+    {
+        return count == capacity;
+    }
+
+    // check if this member collection is empty
+    // Pre-condition: nil
+    // Post-condition: return ture if this member collection is empty; otherwise return false.
+    public bool IsEmpty()
+    {
+        return count == 0;
+    }
+
+    // Add a new member to this member collection
+    // Pre-condition: this member collection is not full
+    // Post-condition: a new member is added to the member collection and the members are sorted in ascending order by their full names;
+    // No duplicate will be added into this the member collection
+    public void Add(IMember member)
+    {
+        // To be implemented by students in Phase 1
+        if (!IsFull())
+        {
+
+            if (count > 0)
+            {
+                if (Search(member) == true)
+                {
+                    Console.WriteLine("{0} has been used by other member", member.ToString());
+                }
+                else
+                {
+                    int pos = count - 1; //initially the value of pos is the index of the last element in the array
+                    while ((pos >= 0) && members[pos].CompareTo((Member)member) > 0)
+                    {
+                        members[pos + 1] = members[pos];
+                        pos--;
+                    }
+                    members[pos + 1] = (Member)member;
+                    count++;
+                    Console.WriteLine("{0} added to member", member.ToString()); ;
+                }
+            }
+            else if (IsEmpty())
+            {
+                members[0] = (Member)member;
+                count++;
+                Console.WriteLine("{0} added to member", member.ToString()); ;
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Membership fully occupied");
+        }
+
+        //For testing
+        //foreach (Member item in members)
+        //{
+        //    if (item != null)
+        //    {
+        //        Console.WriteLine("[{0}]", string.Join(", ", item.ToString())); ;
+        //    }
+        //}
+
+    }
+
+    // Remove a given member out of this member collection
+    // Pre-condition: nil
+    // Post-condition: the given member has been removed from this member collection, if the given meber was in the member collection
+    public void Delete(IMember aMember)
+    {
+        // To be implemented by students in Phase 1
+        int lower = 0;
+        int upper = count - 1;
+        int index = count;
+        while (lower <= upper)
+        {
+            int search = (lower + upper) / 2;
+            if (members[search] != null)
+            {
+                if (members[search].CompareTo((Member)aMember) == 0)
+                {
+                    index = search;
+                    break;
+                }
+                else if (members[search].CompareTo((Member)aMember) < 0)
+                {
+                    lower = search + 1;
+                }
+                else
+                {
+                    upper = search - 1;
+
+                }
+            }
+        }
+        if (index < count)
+        {
+            for (int j = index; j < count; j++)
+                if ((j + 1) >= count)
+                {
+                    members[j] = null;
+                    break;
+                }
+                else if ((j + 1) < count)
+                {
+                    members[j] = members[j + 1];
+
+                }
+            count--;
+            Console.WriteLine("{0} is no longer a member", aMember.ToString());
+        }
+        else
+        {
+            Console.WriteLine("{0} is not a member", aMember.ToString());
+        }
+    }
+
+    // Search a given member in this member collection 
+    // Pre-condition: nil
+    // Post-condition: return true if this memeber is in the member collection; return false otherwise; member collection remains unchanged
+    public bool Search(IMember member)
+    {
+        // To be implemented by students in Phase 1
+        int lower = 0;
+        int upper = count - 1;
+        while (lower <= upper)
+        {
+            int index = (lower + upper) / 2;
+            if (members[index] != null)
+            {
+                if (members[index].CompareTo((Member)member) == 0)
+                {
+                    return true;
+                }
+                else if (members[index].CompareTo((Member)member) < 0)
+                {
+                    lower = index + 1;
+                }
+                else
+                {
+                    upper = index - 1;
+
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    // Remove all the members in this member collection
+    // Pre-condition: nil
+    // Post-condition: no member in this member collection 
+    public void Clear()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            this.members[i] = null;
+        }
+        count = 0;
+    }
+
+    // Return a string containing the information about all the members in this member collection.
+    // The information includes last name, first name and contact number in this order
+    // Pre-condition: nil
+    // Post-condition: a string containing the information about all the members in this member collection is returned
+    public string ToString()
+    {
+        string s = "";
+        for (int i = 0; i < count; i++)
+            s = s + members[i].ToString() + "\n";
+        return s;
+    }
+
+
+}
---
+++ Assessment3/MovieCollection.cs
@@ -0,0 +1,298 @@
+// Phase 2
+// An implementation of MovieCollection ADT
+// 2022
+
+
+using System;
+
+//A class that models a node of a binary search tree
+//An instance of this class is a node in a binary search tree 
+public class BTreeNode
+{
+	private IMovie movie; // movie
+	private BTreeNode lchild; // reference to its left child 
+	private BTreeNode rchild; // reference to its right child
+
+	public BTreeNode(IMovie movie)
+	{
+		this.movie = movie;
+		lchild = null;
+		rchild = null;
+	}
+
+	public IMovie Movie
+	{
+		get { return movie; }
+		set { movie = value; }
+	}
+
+	public BTreeNode LChild
+	{
+		get { return lchild; }
+		set { lchild = value; }
+	}
+
+	public BTreeNode RChild
+	{
+		get { return rchild; }
+		set { rchild = value; }
+	}
+}
+
+// invariant: no duplicates in this movie collection
+public class MovieCollection : IMovieCollection
+{
+	private BTreeNode root; // movies are stored in a binary search tree and the root of the binary search tree is 'root' 
+	private int count; // the number of (different) movies currently stored in this movie collection 
+
+
+
+	// get the number of movies in this movie colllection 
+	// pre-condition: nil
+	// post-condition: return the number of movies in this movie collection and this movie collection remains unchanged
+	public int Number { get { return count; } }
+
+	// constructor - create an object of MovieCollection object
+	public MovieCollection()
+	{
+		root = null;
+		count = 0;	
+	}
+
+	// Check if this movie collection is empty
+	// Pre-condition: nil
+	// Post-condition: return true if this movie collection is empty; otherwise, return false.
+	public bool IsEmpty()
+	{
+		return root == null;
+	}
+
+	// Insert a movie into this movie collection
+	// Pre-condition: nil
+	// Post-condition: the movie has been added into this movie collection and return true, if the movie is not in this movie collection; otherwise, the movie has not been added into this movie collection and return false.
+	public bool Insert(IMovie movie)
+	{
+		if ( root == null && Search(movie) == false)
+        {
+			root = new BTreeNode(movie);
+			count++;
+			return true;
+        }
+        else if( root != null && Search(movie) == false)
+        {
+			Insert(movie, root);
+			count++;
+			return true;
+        }
+        else
+        {
+			return false;
+        }
+	}
+
+	//Private function created to insert item in correct position if the
+	//root is not null
+	//Used in Insert(IMovie movie) function
+	private void Insert(IMovie movie, BTreeNode ptr)
+	{
+		if (movie.CompareTo(ptr.Movie) < 0)
+		{
+			if (ptr.LChild == null)
+				ptr.LChild = new BTreeNode(movie);
+			else
+				Insert(movie, ptr.LChild);
+		}
+		else
+		{
+			if (ptr.RChild == null)
+				ptr.RChild = new BTreeNode(movie);
+			else
+				Insert(movie, ptr.RChild);
+		}
+	}
+
+
+
+	// Delete a movie from this movie collection
+	// Pre-condition: nil
+	// Post-condition: the movie is removed out of this movie collection and return true, if it is in this movie collection; return false, if it is not in this movie collection
+	public bool Delete(IMovie movie)
+	{
+		// search for item and its parent
+		BTreeNode ptr = root; // search reference
+		BTreeNode parent = null; // parent of ptr
+		while ((ptr != null) && (movie.CompareTo(ptr.Movie) != 0))
+		{
+			parent = ptr;
+			if (movie.CompareTo(ptr.Movie) < 0) // move to the left child of ptr
+				ptr = ptr.LChild;
+			else
+				ptr = ptr.RChild;
+		}
+
+		if (ptr != null) // if the search was successful
+		{
+			// case for movie searched has two children
+			if ((ptr.LChild != null) && (ptr.RChild != null))
+			{
+				// find the right-most node in left subtree of ptr
+				if (ptr.LChild.RChild == null) // a special case: the right subtree of ptr.LChild is empty
+				{
+					ptr.Movie = ptr.LChild.Movie;
+					ptr.LChild = ptr.LChild.LChild;
+				}
+				else
+				{
+					BTreeNode p = ptr.LChild;
+					BTreeNode pp = ptr; 
+					while (p.RChild != null)
+					{
+						pp = p;
+						p = p.RChild;
+					}
+					// copy the item at p to ptr
+					ptr.Movie = p.Movie;
+					pp.RChild = p.LChild;
+				}
+			}
+			else // cases for movie searched has no or only one child
+			{
+				BTreeNode c;
+				if (ptr.LChild != null)
+					c = ptr.LChild;
+				else
+					c = ptr.RChild;
+
+				// remove node ptr
+				if (ptr == root) //need to change root
+					root = c;
+				else
+				{
+					if (ptr == parent.LChild)
+						parent.LChild = c;
+					else
+						parent.RChild = c;
+				}				
+			}
+			count--;
+			return true;
+
+		}
+        else// failed to search item
+        {
+			return false;
+        }
+	}
+
+	// Search for a movie in this movie collection
+	// pre: nil
+	// post: return true if the movie is in this movie collection;
+	//	     otherwise, return false.
+	public bool Search(IMovie movie)
+	{
+		return Search(movie, root);
+	}
+
+	//private function created to search for movie we searching for
+	//Used in Search(IMovie movie) function
+	private bool Search(IMovie movie, BTreeNode ptr)
+	{
+		if (ptr != null)
+		{
+			if (movie.CompareTo(ptr.Movie) == 0)
+				return true;
+			else
+				if (movie.CompareTo(ptr.Movie) < 0)
+				return Search(movie, ptr.LChild);
+			else
+				return Search(movie, ptr.RChild);
+		}
+		else
+			return false;
+	}
+
+	// Search for a movie by its title in this movie collection  
+	// pre: nil
+	// post: return the reference of the movie object if the movie is in this movie collection;
+	//	     otherwise, return null.
+	public IMovie Search(string movietitle)
+	{
+		return Search(movietitle, root);
+	}
+
+	//private function created to find for movie
+	//we are searching for with a pointer
+	//Used in Search(string movietitle)
+	private IMovie Search(string movietitle, BTreeNode ptr)
+	{
+		if (ptr != null)
+		{
+			if (movietitle.CompareTo(ptr.Movie.Title) == 0)
+				return ptr.Movie;
+			else
+				if (movietitle.CompareTo(ptr.Movie.Title) < 0)
+				return Search(movietitle, ptr.LChild);
+			else
+				return Search(movietitle, ptr.RChild);
+		}
+		else
+			return null;
+	}
+
+	//global variable to adjust the index for array used in ToArray() function
+	int i = 0;
+
+	// Store all the movies in this movie collection in an array in the dictionary order by their titles
+	// Pre-condition: nil
+	// Post-condition: return an array of movies that are stored in dictionary order by their titles
+	public IMovie[] ToArray()
+	{
+		ResetIndex(ref i);
+		IMovie[] list = new IMovie[Number];
+		InOrderTraverse(root, list);
+		return list;
+	}
+
+	//private function created to perform recursion In-Order Traversal
+	//to set each each element in the array 
+	//Used in ToArray() function
+	private void InOrderTraverse(BTreeNode root, IMovie[] list)
+	{
+		if (root != null)
+		{
+			InOrderTraverse(root.LChild, list);
+			list[i] = root.Movie;	
+			ChangingIndex(ref i);
+			InOrderTraverse(root.RChild, list);
+		}
+	}
+
+	//reference method created to increase index reference to store in array 
+	//in TOArray() function
+	void ChangingIndex(ref int i)
+	{	
+		i++;
+	}
+
+	//reference method created to reset index reference to start the array 
+	//in TOArray() function
+	void ResetIndex(ref int i)
+    {
+		i = 0;
+    }
+
+
+
+	// Clear this movie collection
+	// Pre-condotion: nil
+	// Post-condition: all the movies have been removed from this movie collection 
+	public void Clear()
+	{
+		root = null;
+	}
+}
+
+
+
+
+
