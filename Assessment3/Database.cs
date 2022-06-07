3b6227f61c03ab0d838165db3c534d9797cf1c72
Long Nguyen <npl.long2001@gmail.com>
2022-06-08 01:15:26 +1000

    fixed same problem for add dvd

--- Assessment3/Database.cs
+++ Assessment3/Database.cs
@@ -156,156 +156,165 @@ namespace Assessment3
             //Choose movie title 
             Console.WriteLine("Title of Movie: ");
             string title = Console.ReadLine();
-            //Variable to check if any selection is invalid (e.g duration must be integer)
-            bool option = true;
-            while (option)
+            if (title != "")
             {
-                //CHOOSING GENRE - START
-                Console.WriteLine();
-                Console.WriteLine("Select Genre by Number: ");
-                Console.WriteLine("1. Action");
-                Console.WriteLine("2. Comedy");
-                Console.WriteLine("3. History");
-                Console.WriteLine("4. Drama");
-                Console.WriteLine("5. Western");
-                string genre = Console.ReadLine();
-                option = int.TryParse(genre, out int genreInt);
-                MovieGenre genreChoose;
-                if (genreInt == 1)
+                //Variable to check if any selection is invalid (e.g duration must be integer)
+                bool option = true;
+                while (option)
                 {
-                    genreChoose = MovieGenre.Action;
-                }
-                else if (genreInt == 2)
-                {
-                    genreChoose = MovieGenre.Comedy;
-                }
-                else if (genreInt == 3)
-                {
-                    genreChoose = MovieGenre.History;
-                }
-                else if (genreInt == 4)
-                {
-                    genreChoose = MovieGenre.Drama;
-                }
-                else if (genreInt == 5)
-                {
-                    genreChoose = MovieGenre.Western;
-                }
-                else
-                {
-                    option = false;
-                    break;
-                }
-                //CHOOSING GENRE - END
-                //CHOOSING CLASSIFICATION - START
-                Console.WriteLine();
-                Console.WriteLine("Select Movie Classification by Number:");
-                Console.WriteLine("1. General (G)");
-                Console.WriteLine("2. Parental Guidance (PG)");
-                Console.WriteLine("3. Mature (M)");
-                Console.WriteLine("4. Mature Accompanied (MA15+)");
-                string classify = Console.ReadLine();
-                option = int.TryParse(classify, out int classifyInt);
-                MovieClassification classification;
-                if (classifyInt == 1)
-                {
-                    classification = MovieClassification.G;
-                }
-                else if (classifyInt == 2)
-                {
-                    classification = MovieClassification.PG;
-                }
-                else if (classifyInt == 3)
-                {
-                    classification = MovieClassification.M;
-                }
-                else if (classifyInt == 4)
-                {
-                    classification = MovieClassification.M15Plus;
-                }
-                else
-                {
-                    option = false;
-                    break;
-                }
-                //CHOOSING CLASSIFICATION - END
-                //CHOOSING DURATION - START
-                Console.WriteLine();
-                Console.WriteLine("Duration of Movie:");
-                string duration = Console.ReadLine();
-                option = int.TryParse(duration, out int durationInt);
-                if (!option)
-                {
-                    break;
-                }
-                //CHOOSING DURATION - END
-                //CHOOSE NUMBER OF DVD TO ADD - START
-                Console.WriteLine();
-                Console.WriteLine("Number of DVD to add:");
-                string toAdd = Console.ReadLine();
-                option = int.TryParse(toAdd, out int numberToAdd);
-                if (!option)
-                {
-                    break;
-                }
-                //CHOOSE NUMBER OF DVD TO ADD - END
-
-                while (true)
-                {
-                    //OFFICIALLY ADD IN THE MOVIE 
-                    Console.Clear();
-                    Console.WriteLine("Verify Movie Detail");
-                    Console.WriteLine("Title: " + title + "");
-                    Console.WriteLine("Genre: " + genreChoose + "");
-                    Console.WriteLine("Classification: " + classification + "");
-                    Console.WriteLine("Duration: " + durationInt + "");
-                    Console.WriteLine("Number of DvD: " + numberToAdd + "");
-                    Console.WriteLine("\nAdd this movie in? (Y/N)");
-                    string yesNo = Console.ReadLine();
-                    yesNo = yesNo.ToUpper();
-                    if (yesNo == "Y")
+                    //CHOOSING GENRE - START
+                    Console.WriteLine();
+                    Console.WriteLine("Select Genre by Number: ");
+                    Console.WriteLine("1. Action");
+                    Console.WriteLine("2. Comedy");
+                    Console.WriteLine("3. History");
+                    Console.WriteLine("4. Drama");
+                    Console.WriteLine("5. Western");
+                    string genre = Console.ReadLine();
+                    option = int.TryParse(genre, out int genreInt);
+                    MovieGenre genreChoose;
+                    if (genreInt == 1)
                     {
-                        //Add
-                        Movie movie = new Movie(title, genreChoose, classification, durationInt, numberToAdd);
-                        bool added = movies.Insert(movie);
-                        if (added)
+                        genreChoose = MovieGenre.Action;
+                    }
+                    else if (genreInt == 2)
+                    {
+                        genreChoose = MovieGenre.Comedy;
+                    }
+                    else if (genreInt == 3)
+                    {
+                        genreChoose = MovieGenre.History;
+                    }
+                    else if (genreInt == 4)
+                    {
+                        genreChoose = MovieGenre.Drama;
+                    }
+                    else if (genreInt == 5)
+                    {
+                        genreChoose = MovieGenre.Western;
+                    }
+                    else
+                    {
+                        option = false;
+                        break;
+                    }
+                    //CHOOSING GENRE - END
+                    //CHOOSING CLASSIFICATION - START
+                    Console.WriteLine();
+                    Console.WriteLine("Select Movie Classification by Number:");
+                    Console.WriteLine("1. General (G)");
+                    Console.WriteLine("2. Parental Guidance (PG)");
+                    Console.WriteLine("3. Mature (M)");
+                    Console.WriteLine("4. Mature Accompanied (MA15+)");
+                    string classify = Console.ReadLine();
+                    option = int.TryParse(classify, out int classifyInt);
+                    MovieClassification classification;
+                    if (classifyInt == 1)
+                    {
+                        classification = MovieClassification.G;
+                    }
+                    else if (classifyInt == 2)
+                    {
+                        classification = MovieClassification.PG;
+                    }
+                    else if (classifyInt == 3)
+                    {
+                        classification = MovieClassification.M;
+                    }
+                    else if (classifyInt == 4)
+                    {
+                        classification = MovieClassification.M15Plus;
+                    }
+                    else
+                    {
+                        option = false;
+                        break;
+                    }
+                    //CHOOSING CLASSIFICATION - END
+                    //CHOOSING DURATION - START
+                    Console.WriteLine();
+                    Console.WriteLine("Duration of Movie:");
+                    string duration = Console.ReadLine();
+                    option = int.TryParse(duration, out int durationInt);
+                    if (!option)
+                    {
+                        break;
+                    }
+                    //CHOOSING DURATION - END
+                    //CHOOSE NUMBER OF DVD TO ADD - START
+                    Console.WriteLine();
+                    Console.WriteLine("Number of DVD to add:");
+                    string toAdd = Console.ReadLine();
+                    option = int.TryParse(toAdd, out int numberToAdd);
+                    if (!option)
+                    {
+                        break;
+                    }
+                    //CHOOSE NUMBER OF DVD TO ADD - END
+
+                    while (true)
+                    {
+                        //OFFICIALLY ADD IN THE MOVIE 
+                        Console.Clear();
+                        Console.WriteLine("Verify Movie Detail");
+                        Console.WriteLine("Title: " + title + "");
+                        Console.WriteLine("Genre: " + genreChoose + "");
+                        Console.WriteLine("Classification: " + classification + "");
+                        Console.WriteLine("Duration: " + durationInt + "");
+                        Console.WriteLine("Number of DvD: " + numberToAdd + "");
+                        Console.WriteLine("\nAdd this movie in? (Y/N)");
+                        string yesNo = Console.ReadLine();
+                        yesNo = yesNo.ToUpper();
+                        if (yesNo == "Y")
+                        {
+                            //Add
+                            Movie movie = new Movie(title, genreChoose, classification, durationInt, numberToAdd);
+                            bool added = movies.Insert(movie);
+                            if (added)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Movie successfully added, back to menu...");
+                                displayInfo(movie);
+                                break;
+                            }
+                            else
+                            {
+                                movie = (Movie)movies.Search(title);
+                                movie.TotalCopies += numberToAdd;
+                                movie.AvailableCopies += numberToAdd;
+                                Console.Clear();
+                                Console.WriteLine("Movies already in library, added " + numberToAdd + " DvD instead");
+                                Console.WriteLine("Update!");
+                                displayInfo(movie);
+                                break;
+                            }
+                        }
+                        else if (yesNo == "N")
                         {
                             Console.Clear();
-                            Console.WriteLine("Movie successfully added, back to menu...");
-                            displayInfo(movie);
+                            Console.WriteLine("Back to menu...");
                             break;
                         }
                         else
                         {
-                            movie = (Movie)movies.Search(title);
-                            movie.TotalCopies += numberToAdd;
-                            movie.AvailableCopies += numberToAdd;
                             Console.Clear();
-                            Console.WriteLine("Movies already in library, added " + numberToAdd + " DvD instead");
-                            Console.WriteLine("Update!");
-                            displayInfo(movie);
-                            break;
+                            Console.WriteLine("Invalid option, try again");
                         }
                     }
-                    else if (yesNo == "N")
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Back to menu...");
-                        break;
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Invalid option, try again");
-                    }
+                    break;
                 }
-                break;
+                if (!option)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid selection, back to menu...");
+                }
             }
-            if (!option)
+            else
             {
                 Console.Clear();
-                Console.WriteLine("Invalid selection, back to menu...");
+                Console.WriteLine("Name can't be empty");
             }
+            
         }
 
         //Register a member 
